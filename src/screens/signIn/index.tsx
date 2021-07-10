1f37eaaa838f8521998a26f5abf484683bae41fe
JonatasAlves9 <alvesjonatas99@gmail.com>
2021-07-09 21:05:08 -0300

    Feat: Add folders and some files

---
+++ src/screens/signIn/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export function SignIn() {
+    return (
+        <View >
+            <Text>Open upsa App.tsx to start working on your app!</Text>
+            <Text>Open upsa App.tsx to start working on your app!</Text>
+        </View>
+    );
+}
