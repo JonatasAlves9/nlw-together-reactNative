6917ae30324ae4927351a7307f71c10ede65473f
JonatasAlves9 <alvesjonatas99@gmail.com>
2021-07-10 09:54:10 -0300

    @types for styles globals, tela de signIn end

--- src/screens/signIn/index.tsx
+++ src/screens/signIn/index.tsx
@@ -1,11 +1,43 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import {
+    Text,
+    View,
+    Image,
+    StatusBar
+} from 'react-native';
 import { styles } from './styles'
+import IlustrationImg from '../../assets/illustration.png';
+import { ButtonIcon } from '../../components/ButtonIcon'
 
 export function SignIn() {
     return (
-        <View style={styles.container}>
-            <Text>Open upsa App.tsx to start working on your app!</Text>
-        </View>
+        <>
+            <View style={styles.container}>
+                <StatusBar
+                    barStyle='light-content'
+                    backgroundColor="transparent"
+                    translucent //mostra a barra de status 
+                />
+                <Image
+                    source={IlustrationImg}
+                    style={styles.image}
+                    resizeMethod="resize" />
+
+
+                <View style={styles.content}>
+                    <Text style={styles.title}>
+                        Organize {`\n`}
+                        suas jogatinas {`\n`}
+                        facilmente
+                    </Text>
+                    <Text style={styles.subTitle}>
+                        Crie grupos para jogar seus games {`\n`}
+                        favoritos com seus amigos
+                    </Text>
+                    <ButtonIcon title={'Entrar com o discord'} activeOpacity={.7} />
+                </View>
+            </View>
+
+        </>
     );
 }
