ec807f634ae5a0473319c52d6f735e144e353445
JonatasAlves9 <alvesjonatas99@gmail.com>
2021-07-09 21:14:30 -0300

    Feat: modifications in screens

--- src/screens/signIn/index.tsx
+++ src/screens/signIn/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { styles } from './styles'
 
 export function SignIn() {
     return (
-        <View >
-            <Text>Open upsa App.tsx to start working on your app!</Text>
+        <View style={styles.container}>
             <Text>Open upsa App.tsx to start working on your app!</Text>
         </View>
     );
